80384d5e3d65da82a3e1ba320463970f41a96b6e
chii <>
2023-06-13 00:34:20 +0900

    日本の祝日を取得し、年間カレンダーに反映

--- vue-calendar/src/components/CalendarView.vue
+++ vue-calendar/src/components/CalendarView.vue
@@ -3,12 +3,13 @@ import FullCalendar from "@fullcalendar/vue3";
 import multiMonthPlugin from "@fullcalendar/multimonth";
 import interactionPlugin from "@fullcalendar/interaction";
 import jaLocale from "@fullcalendar/core/locales/ja"; // 日本語化用
+import holidayJp from "@holiday-jp/holiday_jp";
 
 export default {
   components: {
     FullCalendar, // make the <FullCalendar> tag available
   },
-  data: function () {
+  data() {
     return {
       calendarOptions: {
         // カレンダーで使用するプラグイン
@@ -16,7 +17,7 @@ export default {
         initialView: "multiMonthYear",
         // 土日を表示
         weekends: true,
-        events: [{ title: "Meeting", start: new Date() }],
+        events: this.getEvents(),
         locale: jaLocale,
 
         // 数字のみの日付
@@ -25,6 +26,21 @@ export default {
         },
       },
     };
+  },
+  methods: {
+    getEvents() {
+      const currentYear = new Date().getFullYear();
+      const holidays = holidayJp.between(
+        new Date(`${currentYear}-01-01`),
+        new Date(`${currentYear}-12-31`)
+      );
+
+      return holidays.map((holiday) => ({
+        title: holiday.name,
+        start: new Date(holiday.date), // タイムスタンプをDateオブジェクトに変換する
+        allDay: true,
+      }));
+    },
   },
 };
 </script>
